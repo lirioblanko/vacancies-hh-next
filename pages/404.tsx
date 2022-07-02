3270735a8e307a6a3a57364658ca741bc26a8d72
lirioblanko <goldliliya@mail.ru>
2022-07-02 13:07:47 +0300

    404 error (редирект)

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+
 import styles from "../styles/Home.module.scss";
+import { Heading } from "../components/Heading/Heading";
 
 const Error = (): JSX.Element => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/")
+        }, 3000)
+    }, [router])
+
     return (
         <div className={styles.container}>
-            <main className={styles.main}>
-                <h1>404 error</h1>
+            <main className={styles.main + ' ' + styles.main_404 }>
+                <Heading tag={'h1'}>404 error</Heading>
                 <h2>Такой страницы не существует</h2>
             </main>
         </div>
